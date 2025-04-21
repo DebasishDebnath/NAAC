cc9db1d795b3ae0b7fbef3972ddaf1c8621ee5ee
soumibix <soumi.biswas@iemlabs.com>
2025-04-21 14:20:26 +0530

    feat: add UI components for pagination, select, separator, sheet, sidebar, skeleton, and tooltip
    - Implemented Pagination component with navigation links and ellipsis.
    - Created Select component with various subcomponents for dropdown functionality.
    - Added Separator component for visual separation in UI.
    - Developed Sheet component for modal-like overlays.
    - Introduced Sidebar component with collapsible functionality and mobile support.
    - Added Skeleton component for loading placeholders.
    - Implemented Tooltip component for displaying additional information on hover.
    - Created useIsMobile hook for responsive design handling.
    - Added utility functions for class name management.
    - Configured Tailwind CSS and Vite for styling and module resolution.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./components/ui/alert-dialog"
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import './index.css';
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="flex flex-col gap-10 items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-extrabold">Check out components/ui folder</h1>
+      <h1 className="text-3xl font-extrabold">Shadcn UI - Components</h1>
+      <Button variant="default" size="default" className="bg-red-500">
+        Default Button
+      </Button>
+
+      <Input />
+
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
   );
 }
 
