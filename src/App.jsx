d14db31c62e7031fa50a0c8b0fcc655925152903
BiswadipSaha-IEMA <biswadip.saha@iem.edu.in>
2025-04-23 13:56:13 +0530

    added forms and updated layout

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import SuperadminDashboard from "./pages/Superadmin/Dashboard";
 import { ThemeProvider } from "./components/theme/ThemeProvider.jsx";
 import Home from "./pages/User/Home.jsx";
 import Reports from "./pages/User/Reports.jsx";
+import Forms from "./pages/User/Forms.jsx";
 
 // Allowed login roles
 const allowedRoles = ["user", "superadmin", "psudosuperadmin"];
@@ -111,7 +112,7 @@ function App() {
                   }
                 >
                   <Route path="dashboard" element={<Home />} />
-                  <Route path="forms" element={<div>User Forms</div>} />
+                  <Route path="forms" element={<Forms/>} />
                   <Route path="reports" element={<Reports/>} />
                   <Route path="*" element={<NotFound />} />
                   <Route index element={<Navigate to="dashboard" replace />} />
