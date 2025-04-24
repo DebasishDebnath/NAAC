a43d4dda7cfefde3b5408671013e666e293179ae
kunal sen <ksen959595@gmail.com>
2025-04-24 18:07:26 +0530

    notifications added

--- src/pages/User/Notificatons.jsx
+++ src/pages/User/Notificatons.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+import Notification from "../../components/ui/Notifications/Notification.jsx";
+
 const Notificatons = () => {
   return (
-   <div className="w-full bg-red-400">
-<div className="heading text-4xl  text-black font-semibold">Notifications</div>
+    <div className="w-full flex justify-center px-4 sm:px-6 lg:px-8 py-10">
+      <div className="w-full max-w-3xl">
+        <h1 className="text-4xl font-semibold text-black mb-8">Notifications</h1>
+        <div className="flex flex-col gap-3">
+          <Notification title="Welcome!" message="Thanks for joining our platform." time="2h ago" />
+          <Notification title="Update" message="Your profile has been updated successfully." time="4h ago" />
+          <Notification title="Reminder" message="Don't forget your upcoming meeting tomorrow." time="1d ago" />
+          <Notification title="New Feature" message="We've added dark mode to your dashboard!" time="2d ago" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-   </div>
-  )
-}
-export default Notificatons
+export default Notificatons;
