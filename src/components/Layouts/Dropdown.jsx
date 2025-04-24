ea5631c789b0066dfab4f6f6061c36cd8e258b55
kunal sen <ksen959595@gmail.com>
2025-04-24 16:57:39 +0530

    layout change

--- src/components/Layouts/Dropdown.jsx
+++ src/components/Layouts/Dropdown.jsx
@@ -19,7 +19,7 @@ function Dropdown() {
   };
 
   return (
-    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-30">
+    <div className="absolute top-10 right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-30">
       <button
         className="block w-full text-left px-4 py-2 text-sm text-slate-700 hover:bg-slate-100"
         onClick={() => navigate(`${getBasePath()}/profile`)}
