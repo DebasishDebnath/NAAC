e9e601433a7ca2b79f414f834d50d8d077d2409e
Debasish Debnath <ddebnath241@gmail.com>
2025-04-28 13:26:59 +0530

    User Profile Page UI Change like Figma

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ import PreviewSubmit from "./pages/User/PreviewSubmit.jsx";
 // Flag to control route protection
 // When set to true: Protected routes are enforced (normal security behavior)
 // When set to false: All routes are accessible without authentication/authorization
-const ENFORCE_ROUTE_PROTECTION = true;
+const ENFORCE_ROUTE_PROTECTION = false;
 
 // Allowed login roles
 const allowedRoles = ["user", "superadmin", "psudosuperadmin"];
