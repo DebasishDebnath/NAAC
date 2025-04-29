1882b37fce284837305161f5a706527728e50028
AkashBag2004 <akash.bag@iemlabs.com>
2025-04-29 14:56:46 +0530

    Fetch draft data

--- src/pages/User/Drafts.jsx
+++ src/pages/User/Drafts.jsx
@@ -1,10 +1,27 @@
 import DraftsTable from '@/components/Drafts/DraftsTable'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useHttp } from '@/hooks/useHttp'
 
 const Drafts = () => {
+  const { getReq } = useHttp();
+  const accessToken = sessionStorage.getItem("token");
+  // console.log(accessToken)
+  useEffect(() => {
+    const fetchJournals = async () => {
+      try {
+        const json = await getReq(`api/v2/user/tempContribution`, accessToken);
+        console.log(json)
+      } catch (error) {
+        console.log("Error fetching journal publication!", error);
+      }
+    }
+    fetchJournals()
+  }, [])
+
+
   return (
     <div >
-      <DraftsTable/>
+      <DraftsTable />
     </div>
   )
 }
